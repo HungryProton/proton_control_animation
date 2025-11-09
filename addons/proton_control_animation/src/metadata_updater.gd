cdde4f777bd7344fc1fcbee30a0131e417f76e74
HungryProton <hungryproton@pm.me>
2025-11-09 23:20:08 +0100

    update initial metadata even if the control is hidden when it's a child of a container, fixes #4

--- addons/proton_control_animation/src/metadata_updater.gd
+++ addons/proton_control_animation/src/metadata_updater.gd
@@ -38,7 +38,7 @@ func _find_parent_container(root: Control) -> Container:
 
 
 func _update_metadata(full_state: bool = false) -> void:
-	if not target.is_visible_in_tree():
+	if not _can_update():
 		return
 
 	target.set_meta(ProtonControlAnimation.META_ORIGINAL_POSITION, target.position)
@@ -52,6 +52,22 @@ func _update_metadata(full_state: bool = false) -> void:
 		target.set_meta(ProtonControlAnimation.META_ORIGINAL_SELF_MODULATE, target.self_modulate)
 		target.set_meta(ProtonControlAnimation.META_ORIGINAL_SIZE, target.size)
 	updated.emit()
+
+
+## Returns true if the target is in its final position.
+## /!\ Update this on a case by case basis /!\
+##
+## If the target is the direct child of a container, it should be fine to update even if it's not
+## visible (that's required for tab containers for example).
+## In the case of a control placed with anchors though, it breaks, and I don't remember why.
+## (Spamming the 'Side Panel' button in the 00_showcase scene breaks if the metadata is updated
+## when the control is hidden)
+func _can_update() -> bool:
+	if not target.is_node_ready():
+		return false
+	if not target.get_parent() is Container:
+		return target.is_visible_in_tree()
+	return true
 
 
 func _is_animation_in_progress() -> bool:
